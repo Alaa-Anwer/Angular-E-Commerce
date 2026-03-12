636544a94f15a761d9d5f240126278daacd9fecf
Alaa Anwer <anwaralaa286@gmail.com>
2026-03-12 16:47:52 +0200

    feat: add home contact component

---
+++ src/app/components/home-contact/home-contact.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-contact',
+  imports: [],
+  templateUrl: './home-contact.html',
+  styleUrl: './home-contact.css',
+})
+export class HomeContact {}
