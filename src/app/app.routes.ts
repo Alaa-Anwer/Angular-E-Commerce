abde5e7ac32a2d06a01ae5836920e50abb91a738
Alaa Anwer <anwaralaa286@gmail.com>
2026-03-12 16:48:22 +0200

    update: add routing for contact component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Slider } from './components/slider/slider';
+import { HomeProducts } from './components/home-products/home-products';
+import { HomeContact } from './components/home-contact/home-contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: 'home', component: Slider },
+
+  { path: 'products', component: HomeProducts },
+
+  { path: 'contact', component: HomeContact },
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+
+];
